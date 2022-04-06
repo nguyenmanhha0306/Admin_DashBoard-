3aef6ac0eb86e1755089fc3e56c64e121d9bea48
Ha <ha030600@gmail.com>
2022-04-06 22:34:19 +0700

    update Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import Chart from "react-apexcharts";
+
 import Statuscard from "../components/status-card/Statuscard";
 
 import statusCards from "../assets/JsonData/status-card-data.json";
+
+const chartOptions = {
+  series: [
+    {
+      name: "Online Customers",
+      data: [40, 70, 20, 90, 30, 60, 23, 32, 78],
+    },
+    {
+      name: "Store Customers",
+      data: [34, 65, 76, 31, 76, 98, 43, 56, 97],
+    },
+  ],
+  options: {
+    color: ["#6ab04c", "#2980b9"],
+    chart: {
+      background: "transparent",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May ",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+      ],
+    },
+    legend: {
+      position: "bottom",
+    },
+    gird: {
+      show: false,
+    },
+  },
+};
+
 const Dashboard = () => {
   return (
     <div>
@@ -12,8 +61,6 @@ const Dashboard = () => {
           <div className="row">
             {statusCards.map((item, index) => (
               <div className="col-6">
-                {/* status card here */}
-                {item.title}
                 <Statuscard
                   icon={item.icon}
                   count={item.count}
@@ -21,6 +68,28 @@ const Dashboard = () => {
                 />
               </div>
             ))}
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card full-height">
+            {/* Chart */}
+            <Chart
+              options={chartOptions.options}
+              series={chartOptions.series}
+              type="line"
+              height="100%"
+            />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card__header">
+              <h3>top customers</h3>
+            </div>
+            <div className="card__body">{/* table */}</div>
+            <div className="card__footer">
+              <Link to="/">View all</Link>
+            </div>
           </div>
         </div>
       </div>

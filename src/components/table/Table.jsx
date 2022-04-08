f3e33aea3668c3f007a527bd633c528880a1f536
Ha <ha030600@gmail.com>
2022-04-08 21:35:51 +0700

    update Table Customer

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./table.css";
 
 const Table = (props) => {
+  const initDateShow =
+    props.limit && props.bodyData
+      ? props.bodyData.slice(0, Number(props.limit))
+      : props.bodyData;
+
+  const [dataShow, setDataShow] = useState(initDateShow);
+
+  let pages = 1;
+
+  let range = [];
+
+  if (props.limit !== undefined) {
+    let page = Math.floor(props.bodyData.length / Number(props.limit));
+    pages = props.bodyData.legend % Number(props.limit) === 0 ? page : page + 1;
+    range = [...Array(pages).keys()];
+  }
+
+  const [currPage, setCurrPage] = useState(0);
+
+  const selectPage = (page) => {
+    const start = Number(props.limit) * page;
+    const end = start + Number(props.limit);
+
+    setDataShow(props.bodyData.slice(start, end));
+
+    setCurrPage(page);
+  };
+
   return (
     <div>
       <div className="table-wrapper">
@@ -18,13 +46,26 @@ const Table = (props) => {
           ) : null}
           {props.bodyData && props.renderBody ? (
             <tbody>
-              {props.bodyData.map((item, index) =>
-                props.renderBody(item, index)
-              )}
+              {dataShow.map((item, index) => props.renderBody(item, index))}
             </tbody>
           ) : null}
         </table>
       </div>
+      {pages > 1 ? (
+        <div className="table__pagination">
+          {range.map((item, index) => (
+            <div
+              key={index}
+              className={`table__pagination-item ${
+                currPage === index ? "active" : ""
+              }`}
+              onClick={() => selectPage(index)}
+            >
+              {item + 1}
+            </div>
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 };
